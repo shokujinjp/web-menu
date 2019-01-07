ab7459792f353693c2e219f1b6e01de487012bfc
whywaita <whywaita@whywrite.it>
2019-01-07 13:40:53 +0900

    for smartphone, can not display huge modal

--- src/modal.tsx
+++ src/modal.tsx
@@ -21,7 +21,6 @@ function getModalStyle() {
 const styles = theme => ({
   paper: {
     position: "absolute",
-    width: theme.spacing.unit * 50,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
